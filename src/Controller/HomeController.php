8c30b7225c13e6e532a292ede6cd8c58fd402101
laura-enya-galindo <nancy.drew.lg@gmail.com>
2023-02-10 11:17:45 +0100

    Correction de l'affichage du formulaire user

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,9 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-use App\Entity\User;
-use App\Form\UserType;
-
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
@@ -16,20 +13,6 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-        ]);
-    }
-
-    /**
-    *@Route("/victorious/elephant/book", name="app_victorious_elephant_test")
-    */
-    public function editUser(): Response
-    {
-        $user = new User();  
-        $form = $this->createForm(UserType::class, $user);
-
-        return $this->render('user.html.twig',
-        [
-            'userForm' => $form->createView(),
         ]);
     }
 }

3b2bdc5ae6d0f90353687bfdf8e310553171dd81
laura-enya-galindo <nancy.drew.lg@gmail.com>
2023-02-10 09:55:07 +0100

    Affichage du formulaire fini

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\User;
+use App\Form\UserType;
+
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]

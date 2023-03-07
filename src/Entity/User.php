1c0960b8d6d345888695397a0f4dbbef075d10f4
laura-enya-galindo <nancy.drew.lg@gmail.com>
2023-03-07 18:52:56 +0100

    Création du Login Controller

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -10,9 +10,13 @@ use Doctrine\ORM\Mapping as ORM;
 // Pour pouvoir ajouter des contraintes sur les input dans le formulaire UserType sur UserController
 use Symfony\Component\Validator\Constraints as Assert;
 
+// Pour créer le login
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
-class User
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]

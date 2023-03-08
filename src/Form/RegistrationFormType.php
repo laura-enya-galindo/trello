8d9cc41933663ed206326ea56a899955391c2044
laura-enya-galindo <nancy.drew.lg@gmail.com>
2023-03-08 21:42:03 +0100

    Authentification d'un utilisateur terminée

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,31 +4,31 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Mapping\ClassMetadata;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', [
+            ->add('email', EmailType::class, [
                'constraints' => [
                     new NotBlank([
                         'message' => 'Vous devez entrer une adresse email',
                     ]),
-                    new Email([
-                        'message' => "L'adresse email {{ value }} n'est pas valide",
-                    ]),
-                ] 
+                ],
+                'invalid_message' => "L'adresse email {{ value }} n'est pas valide",
             ])
-            ->add('first_name', [
+            ->add('first_name', TextType::class, [
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Vous devez entrer un prénom',
@@ -41,7 +41,7 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ]
             ])
-            ->add('last_name', [
+            ->add('last_name', TextType::class, [
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Vous devez entrer un nom de famille',
@@ -54,6 +54,7 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ]
             ])
+            # TO-DO : mettre un RepeatedType field
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller

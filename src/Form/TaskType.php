d507346ea43593d37f8beee7c07f046b1c06e431
laura-enya-galindo <nancy.drew.lg@gmail.com>
2023-03-09 11:32:37 +0100

    Création du formulaire (tout début)

---
+++ src/Form/TaskType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Task;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('content')
+            ->add('created_at')
+            ->add('updated_at')
+            ->add('completed_at')
+            ->add('users')
+            ->add('status')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Task::class,
+        ]);
+    }
+}

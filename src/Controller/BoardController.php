61990f99ed1490d1a602efc5205820747b81e49a
laura-enya-galindo <nancy.drew.lg@gmail.com>
2023-03-19 20:11:52 +0100

    Il manque maintenant uniquement les colonnes « Statut » et « Utilisateur(s) » au tableau de BoardController

--- src/Controller/BoardController.php
+++ src/Controller/BoardController.php
@@ -7,6 +7,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 use App\Repository\TaskRepository;
+// use App\Repository\UserRepository;
+// use App\Repository\StatusRepository;
+
 
 class BoardController extends AbstractController
 {
@@ -14,9 +17,13 @@ class BoardController extends AbstractController
     public function index(TaskRepository $taskRepository): Response
     {
         $task = $taskRepository->findAll();
+        // $users = $userRepository->findAll();
+        // $status = $statusRepository->findAll();
 
         return $this->render('board/index.html.twig', [
             'task' => $task,
+            // 'status' => $status,
+            // 'users' => $users,
         ]);
     }
 }

a5931834735b983f8826bd62518c5ed74fba7e27
laura-enya-galindo <nancy.drew.lg@gmail.com>
2023-03-21 22:48:42 +0100

    affichage du statut dans le board ajouté

--- src/Controller/BoardController.php
+++ src/Controller/BoardController.php
@@ -7,8 +7,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 use App\Repository\TaskRepository;
-// use App\Repository\UserRepository;
-// use App\Repository\StatusRepository;
 
 
 class BoardController extends AbstractController
@@ -17,8 +15,6 @@ class BoardController extends AbstractController
     public function index(TaskRepository $taskRepository): Response
     {
         $task = $taskRepository->findAll();
-        // $users = $userRepository->findAll();
-        // $status = $statusRepository->findAll();
 
         return $this->render('board/index.html.twig', [
             'task' => $task,
